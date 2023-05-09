3f94e450db452e5c7651397b887469a668c42419
LeShaDDoW <leshaddow3@gmail.com>
2023-05-09 18:48:51 +0200

    level 1 polish

---
+++ Assets/Backrooms Mod/Scripts/thing.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ModWobblyLife;
+
+public class thing : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
