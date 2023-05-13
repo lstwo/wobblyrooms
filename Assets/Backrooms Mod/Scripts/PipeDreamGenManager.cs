889634731e0a9886495dcabc164cba3700fd727e
LeShaDDoW <leshaddow3@gmail.com>
2023-05-13 16:41:31 +0200

    changed export Settings

--- Assets/Backrooms Mod/Scripts/PipeDreamGenManager.cs
+++ Assets/Backrooms Mod/Scripts/PipeDreamGenManager.cs
@@ -5,5 +5,15 @@ using ModWobblyLife;
 
 public class PipeDreamGenManager : MonoBehaviour
 {
-    
+    public Transform spawnPos;
+    public GameObject roomObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(roomObj, spawnPos.position, roomObj.transform.rotation);
+            gameObject.SetActive(false);
+        }
+    }
 }
