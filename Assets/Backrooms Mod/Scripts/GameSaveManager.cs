bd5f148b578712001170ff1e124a7dfcba3fb8d9
LeShaDDoW <leshaddow3@gmail.com>
2023-06-21 16:48:16 +0200

    continued saving

--- Assets/Backrooms Mod/Scripts/GameSaveManager.cs
+++ Assets/Backrooms Mod/Scripts/GameSaveManager.cs
@@ -7,13 +7,23 @@ using TMPro;
 
 public class GameSaveManager : MonoBehaviour
 {
-    public Image gameImmage;
-    public TextMeshProUGUI gameName;
+    public Image gameImage;
+    public Text gameName;
     public int saveNumber;
+    public AssignLists imageAssigns;
+
+    GameSave save = new GameSave("unknown_save");
 
     private void Awake()
     {
+        if (saveNumber == 1) save = GameSaver.save1; else if (saveNumber == 2) save = GameSaver.save2; else if (saveNumber == 3) save = GameSaver.save3;
+        gameImage.sprite = imageAssigns.spriteList[save.level];
+        gameName.text = save.name;
+    }
 
+    void LoadSave()
+    {
+        GameSaver.currentSave = saveNumber;
     }
 }
 
@@ -23,23 +33,48 @@ public static class GameSaver
     public static GameSave save2 = new GameSave("save_two");
     public static GameSave save3 = new GameSave("save_three");
 
-    public static void SaveGame(GameSave save, int level)
+    public static int currentSave = 1;
+
+    public static void SaveGame(GameSave save, int level, string name, int seed)
     {
         save.level = level;
+        save.name = name;
+        save.seed = seed;
         PlayerPrefs.SetInt(save.pprefsKeyPrefix + "_level", save.level);
+        PlayerPrefs.SetString(save.pprefsKeyPrefix + "_name", save.name);
+        PlayerPrefs.SetInt(save.pprefsKeyPrefix + "_seed", save.seed);
+
     }
 
-    public static void LoadGame(GameSave save)
+    public static void LoadSave(GameSave save)
     {
+        save.level = PlayerPrefs.GetInt(save.pprefsKeyPrefix + "_level");
+        save.name = PlayerPrefs.GetString(save.pprefsKeyPrefix + "_name");
         SceneManager.LoadScene("Level " + save.level);
+    }
+
+    public static void UpdateSaveGet(GameSave save)
+    {
+        save.level = PlayerPrefs.GetInt(save.pprefsKeyPrefix + "_level");
+        save.name = PlayerPrefs.GetString(save.pprefsKeyPrefix + "_name");
+        save.seed = PlayerPrefs.GetInt(save.pprefsKeyPrefix + "_seed");
+    }
+
+    public static void UpdateSaveSet(GameSave save)
+    {
+        PlayerPrefs.SetInt(save.pprefsKeyPrefix + "_level", save.level);
+        PlayerPrefs.SetString(save.pprefsKeyPrefix + "_name", save.name);
+        PlayerPrefs.SetInt(save.pprefsKeyPrefix + "_seed", save.seed);
     }
 }
 
 public class GameSave
 {
-    public string pprefsKeyPrefix;
+    public string pprefsKeyPrefix = "default_save_name";
+    public string name = "Unnamed Save";
 
-    public int level;
+    public int level = 0;
+    public int seed = 0;
 
     public GameSave(string pprefsKeyPrefix)
     {
