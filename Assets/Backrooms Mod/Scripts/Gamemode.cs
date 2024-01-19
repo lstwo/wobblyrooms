1af8622605240764d3baaf130faad50837e0f315
LeShaDDoW <leshaddow3@gmail.com>
2024-01-19 19:54:54 +0100

    quick hot fix

--- Assets/Backrooms Mod/Scripts/Gamemode.cs
+++ Assets/Backrooms Mod/Scripts/Gamemode.cs
@@ -13,9 +13,12 @@ public class Gamemode : ModFreemodeGamemode
 
     private void Start()
     {
+        instance.jumpscares = PlayerPrefs.GetInt("jumpscares") == 1;
         DebugLogConsole.AddCommand("backrooms.changeLevel", "Changes the Level you are currently in.", (Action<int>)ChangeLevel, "LevelNumber");
         DebugLogConsole.AddCommand("backrooms.newSeed", "Generates a new Seed for the random generation.", ResetSeed);
         DebugLogConsole.AddCommand("backrooms.jumpscares", "Set whether or not to use jumpscares.", (Action<bool>)SetJumpscares, "EnableJumpscares");
+        DebugLogConsole.AddCommand("backrooms.jumpscares", "Set whether or not to use jumpscares.", GetJumpscares);
+
         SceneManager.activeSceneChanged += ResetConsoleCommands;
     }
 
@@ -34,6 +37,13 @@ public class Gamemode : ModFreemodeGamemode
     static void SetJumpscares(bool b)
     {
         instance.jumpscares = b;
+        if(b) PlayerPrefs.SetInt("jumpscares", 1);
+        else PlayerPrefs.SetInt("jumpscares", 0);
+    }
+
+    static void GetJumpscares()
+    {
+        Debug.Log(instance.jumpscares);
     }
 
     static void ResetConsoleCommands(Scene a, Scene b)
